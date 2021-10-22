234dc8aff0998e6098ddd03b093074ff5ba3a67f
Alex Hoyau <a.hoyau@silexlabs.org>
2021-10-22 22:23:08 +0200

    Remove __aaa

--- __aaa/js/script.js
+++
@@ -1,8 +0,0 @@
-
-$(function() {
-    $('.hero-scroll-down').click(function() {
-        $([document.documentElement, document.body]).animate({
-            scrollTop: $("#after-hero").offset().top
-        }, 500);
-    });
-})
