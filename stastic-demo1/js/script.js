71d09d590c7020ce0dae1abc2e5ff64fdf78c4ff
Alex Hoyau <a.hoyau@silexlabs.org>
2022-02-17 15:32:34 +0100

    Batch update

---
+++ stastic-demo1/js/script.js
@@ -0,0 +1,4 @@
+
+
+    window.silex = window.silex || {}
+    window.silex.data = {"site":{"width":1200},"pages":[{"id":"page-page-1","displayName":"Page 1","link":{"linkType":"LinkTypePage","href":"#!page-page-1"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
